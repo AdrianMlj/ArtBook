62911b13c8600736bdfd16666155fb1ec50a04e2
AdrianMlj <adriannomialinjakaniaina@gmail.com>
2025-03-19 16:12:55 +0300

    Update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const CONFIG = {
-    ACCESS_TOKEN: process.env.NEXT_PUBLIC_GOOGLE_ACCESS_TOKEN, // Vercel environment variable
-    PROPERTY_ID: process.env.NEXT_PUBLIC_GOOGLE_PROPERTY_ID,   // Vercel environment variable
+    PROPERTY_ID: process.env.NEXT_PUBLIC_GOOGLE_PROPERTY_ID,   // Vercel environment variable for property ID
 };
 
-window.CONFIG = CONFIG; // Make CONFIG globally available
+// Make CONFIG globally available for use
+window.CONFIG = CONFIG;
